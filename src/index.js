1b5a5f61b6ea0177e3b2b56cba5a717f1c3b8ab3
Jefferson Santos <doctorjeffbr@yahoo.com.br>
2025-08-01 16:58:40 -0300

    convertendo escrita do código para a língua inglesa

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 const player01 = {
     name: "Mario",
-    velocidade: 4,
-    manobrabilidade: 3,
-    poder: 3,
-    pontos: 0
+    speed: 4,
+    maneuverability: 3,
+    power: 3,
+    score: 0
 }
 
 const player02 = {
     name: "Luigi",
-    velocidade: 3,
-    manobrabilidade: 3,
-    poder: 4,
-    pontos: 0
+    speed: 3,
+    maneuverability: 3,
+    power: 4,
+    score: 0
 }
 
 async function rollDice() {
@@ -24,13 +24,13 @@ async function getRandomBlock() {
 
     switch (true) {
         case random < 0.33:
-            result = "RETA"            
+            result = "STRAIGHT-LINE"            
             break;
         case random < 0.66:
-            result = "CURVA"
+            result = "CURVE"
             break;   
         default:
-            result = "CONFRONTO"
+            result = "FIGHT"
     }
     return result;
 }
@@ -64,78 +64,78 @@ async function playRaceEngine(character01, character02) {
         function checkBlockWinner() {
             if (totalTestSkill01 > totalTestSkill02) {
                 console.log(`${character01.name} marcou 1 ponto!"`);
-                character01.pontos++;
+                character01.score++;
             } else if (totalTestSkill02 > totalTestSkill01) {
                 console.log(`${character02.name} marcou 1 ponto!`);
-                character02.pontos++;
+                character02.score++;
             } else if (totalTestSkill01 === totalTestSkill02) {
                 console.log("Empate: nenhum ponto foi marcado nesta rodada!");
             }
         }
 
-        if (block === "RETA") {
-            totalTestSkill01 = diceResult01 + character01.velocidade
-            totalTestSkill02 = diceResult02 + character02.velocidade
+        if (block === "STRAIGHT-LINE") {
+            totalTestSkill01 = diceResult01 + character01.speed
+            totalTestSkill02 = diceResult02 + character02.speed
 
-            await logRollResult(character01.name, "velocidade", diceResult01, character01.velocidade);
-            await logRollResult(character02.name, "velocidade", diceResult02, character02.velocidade);
+            await logRollResult(character01.name, "velocidade", diceResult01, character01.speed);
+            await logRollResult(character02.name, "velocidade", diceResult02, character02.speed);
 
             checkBlockWinner();
         }
     
-        if (block === "CURVA") {
-            totalTestSkill01 = diceResult01 + character01.manobrabilidade
-            totalTestSkill02 = diceResult02 + character02.manobrabilidade
+        if (block === "CURVE") {
+            totalTestSkill01 = diceResult01 + character01.maneuverability
+            totalTestSkill02 = diceResult02 + character02.maneuverability
 
-            await logRollResult(character01.name, "manobrabilidade", diceResult01, character01.manobrabilidade);
-            await logRollResult(character02.name, "manobrabilidade", diceResult02, character02.manobrabilidade);
+            await logRollResult(character01.name, "manobrabilidade", diceResult01, character01.maneuverability);
+            await logRollResult(character02.name, "manobrabilidade", diceResult02, character02.maneuverability);
             
             checkBlockWinner();
         }
     
-        if (block === "CONFRONTO") {
+        if (block === "FIGHT") {
             
-            let powerResult01 = diceResult01 + character01.poder
-            let powerResult02 = diceResult02 + character02.poder
+            let powerResult01 = diceResult01 + character01.power
+            let powerResult02 = diceResult02 + character02.power
             let surpriseElement = await getSurpriseElement()
 
             console.log(`${character01.name} confrontou ${character02.name}! 🥊`);
             console.log("Se elemento supresa = 🐢 (casco) -> perdedor perde 1 ponto")
             console.log("Se elemento supresa = 💣 (bomba) -> perdedor perde 2 pontos")
 
-            await logRollResult(character01.name, "poder", diceResult01, character01.poder);
-            await logRollResult(character02.name, "poder", diceResult02, character02.poder);
+            await logRollResult(character01.name, "poder", diceResult01, character01.power);
+            await logRollResult(character02.name, "poder", diceResult02, character02.power);
 
             console.log(`Elemento supresa: ${surpriseElement}`);
 
             //verificando o vencedor do confronto
             if (powerResult01 > powerResult02) {
-                if (character02.pontos > 0 && surpriseElement === "casco!") {
+                if (character02.score > 0 && surpriseElement === "casco!") {
                     console.log(`${character01.name} venceu o confronto e ganhou bônus de 1 ponto! ${character02.name} perdeu 1 ponto! 🐢`);
-                    character01.pontos++;
-                    character02.pontos--;
-                } else if (character02.pontos > 1 && surpriseElement === "bomba!") {
+                    character01.score++;
+                    character02.score--;
+                } else if (character02.score > 1 && surpriseElement === "bomba!") {
                     console.log(`${character01.name} venceu o confronto e ganhou bônus de 1 ponto! ${character02.name} perdeu 2 pontos! 🐢`);
-                    character01.pontos++;
-                    character02.pontos -= 2;
-                } else if (character02.pontos === 0) {
+                    character01.score++;
+                    character02.score -= 2;
+                } else if (character02.score === 0) {
                     console.log(`${character01.name} venceu o confronto e ganhou bônus de 1 ponto! Contudo, ${character02.name} não tem pontos para perder! 🐢`);
-                    character01.pontos++;
+                    character01.score++;
                 }            
             }
 
             if (powerResult02 > powerResult01) {
-                if (character01.pontos > 0 && surpriseElement === "casco!") {
+                if (character01.score > 0 && surpriseElement === "casco!") {
                     console.log(`${character02.name} venceu o confronto e ganhou bônus de 1 ponto! ${character01.name} perdeu um ponto! 🐢`);
-                    character02.pontos++;
-                    character01.pontos--;
-                }else if (character01.pontos > 1 && surpriseElement === "bomba!") {
+                    character02.score++;
+                    character01.score--;
+                }else if (character01.score > 1 && surpriseElement === "bomba!") {
                     console.log(`${character02.name} venceu o confronto e ganhou bônus de 1 ponto! ${character01.name} perdeu 2 pontos! 🐢`);
-                    character02.pontos++;
-                    character01.pontos -= 2;
-                }else if (character01.pontos === 0) {
+                    character02.score++;
+                    character01.score -= 2;
+                }else if (character01.score === 0) {
                     console.log(`${character02.name} venceu o confronto e ganhou bônus de 1 ponto! Contudo, ${character01.name} não tem pontos para perder! 🐢`);
-                    character02.pontos++;
+                    character02.score++;
                 }
             }
 
@@ -152,12 +152,12 @@ async function playRaceEngine(character01, character02) {
 //declarando o vencedor da corrida
 async function declareWinner (character01, character02) {
     console.log("Resultado final:");
-    console.log(`${character01.name}: ${character01.pontos} ponto(s)`);
-    console.log(`${character02.name}: ${character02.pontos} ponto(s)`);
+    console.log(`${character01.name}: ${character01.score} ponto(s)`);
+    console.log(`${character02.name}: ${character02.score} ponto(s)`);
 
-    if (character01.pontos > character02.pontos) // quando tenho vários ifs concatenados, posso excluir os blocos "{}" que os encapsulam.
+    if (character01.score > character02.score)
         console.log(`\n${character01.name} venceu a corrida, parabéns! 🏆`);
-    else if (character02.pontos > character01.pontos)
+    else if (character02.score > character01.score)
         console.log(`\n${character02.name} venceu a corrida, parabéns! 🏆`);
     else
         console.log("A corrida terminou empatada! Nenhum vencedor!");
